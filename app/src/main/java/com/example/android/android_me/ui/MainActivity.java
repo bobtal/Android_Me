074ac80c02aa6ecbd51e7e5c115293487ac588f1
bobtal <talevski.boban@gmail.com>
2018-10-07 18:00:38 +0200

    Completes exercise TFragments.06

--- app/src/main/java/com/example/android/android_me/ui/MainActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -16,8 +16,11 @@
 
 package com.example.android.android_me.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.android.android_me.R;
@@ -26,6 +29,15 @@ import com.example.android.android_me.R;
 // Implement the MasterListFragment callback, OnImageClickListener
 public class MainActivity extends AppCompatActivity implements MasterListFragment.OnImageClickListener{
 
+    public static final String HEAD_INDEX_EXTRA = "head_index";
+    public static final String BODY_INDEX_EXTRA = "body_index";
+    public static final String LEG_INDEX_EXTRA = "leg_index";
+
+    // Variables to store the values for the list index of the selected images
+    // The default value will be index = 0
+    private int headIndex;
+    private int bodyIndex;
+    private int legIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +51,44 @@ public class MainActivity extends AppCompatActivity implements MasterListFragmen
         // Create a Toast that displays the position that was clicked
         Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
 
-        // TODO (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        // COMPLETED (2) Based on where a user has clicked, store the selected list index for the head, body, and leg BodyPartFragments
+        // bodyPartNumber will be = 0 for the head fragment, 1 for the body, and 2 for the leg fragment
+        // Dividing by 12 gives us these integer values because each list of images resources has a size of 12
+        int bodyPartNumber = position / 12;
 
-        // TODO (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        // Store the correct list index no matter where in the image list has been clicked
+        // This ensures that the index will always be a value between 0-11
+        int listIndex = position - 12 * bodyPartNumber;
 
-        // TODO (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        // Set the currently displayed item for the correct body part fragment
+        switch (bodyPartNumber) {
+            case 0: headIndex = listIndex;
+                break;
+            case 1: bodyIndex = listIndex;
+                break;
+            case 2: legIndex = listIndex;
+                break;
+            default: break;
+        }
+
+        // COMPLETED (3) Put this information in a Bundle and attach it to an Intent that will launch an AndroidMeActivity
+        Bundle bundle = new Bundle();
+        bundle.putInt(HEAD_INDEX_EXTRA, headIndex);
+        bundle.putInt(BODY_INDEX_EXTRA, bodyIndex);
+        bundle.putInt(LEG_INDEX_EXTRA, legIndex);
+
+        // Attache the bundle to an intent
+        final Intent intent = new Intent(this, AndroidMeActivity.class);
+        intent.putExtras(bundle);
+
+        // COMPLETED (4) Get a reference to the "Next" button and launch the intent when this button is clicked
+        Button nextButton = (Button) findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
 
     }
 
